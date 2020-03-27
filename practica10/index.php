65d9840b4c17edc54d15b9d99d27fdd269b56d91
german230120 <60245389+german230120@users.noreply.github.com>
2020-03-27 01:16:21 -0600

    Add files via upload

---
+++ practica10/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Practica10</title>
+    <link rel="stylesheet" href="css/style.css">
+    <body background="https://i.imgur.com/45pfM1B.jpg?fb">
+ 
+
+</head>
+<body>
+
+    <?php
+            Echo "<br><br><br><br><br><br></br></br></br></br></br></br>";
+ 
+            Echo "<center><h1>HOLA MUNDO</h1><center/>";
+           
+    ?>
+
+</body>
+</html>
